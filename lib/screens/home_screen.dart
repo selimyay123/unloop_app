225d796f4eb7bf66dd4709f532d29005c4c00d4a
Selim Yay <selimyay@192.168.1.4>
2026-02-01 14:33:31 -0800

    added bottom navbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   final List<Map<String, dynamic>> _targetApps = [
     {"name": "Instagram", "icon": Icons.camera_alt, "isBlocked": true},
     {"name": "TikTok", "icon": Icons.music_note, "isBlocked": false},
@@ -25,39 +33,55 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(title: Text("Unloop")),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              HeroSection(
-                isProtectionActive: _isProtectionActive,
-                onModeChanged: (newValue) {
-                  setState(() {
-                    _isProtectionActive = newValue;
-                  });
-                },
+      body: _selectedIndex == 0
+          ? Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/background.png"),
+                  fit: BoxFit.cover,
+                ),
               ),
-              StatCardSection(),
-              WeeklyReportSection(),
-              AppListSection(
-                targetApps: _targetApps,
-                onAppToggled: (index, currentValue) {
-                  setState(() {
-                    _targetApps[index]['isBlocked'] = currentValue;
-                  });
-                },
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroSection(
+                      isProtectionActive: _isProtectionActive,
+                      onModeChanged: (newValue) {
+                        setState(() {
+                          _isProtectionActive = newValue;
+                        });
+                      },
+                    ),
+                    StatCardSection(),
+                    WeeklyReportSection(),
+                    AppListSection(
+                      targetApps: _targetApps,
+                      onAppToggled: (index, currentValue) {
+                        setState(() {
+                          _targetApps[index]['isBlocked'] = currentValue;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ],
+            )
+          : Center(child: Text("Settings Screen")),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
           ),
-        ),
+        ],
       ),
     );
   }
